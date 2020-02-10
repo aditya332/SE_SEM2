df445f75e646aed211efb3b415544d9f9f572024
aditya332 <50880895+aditya332@users.noreply.github.com>
2020-02-10 21:01:46 +0530

    till assgn05

--- 21162_ADSL/assgn05-1/src/assgn05-1.cpp
+++ 21162_ADSL/assgn05-1/src/assgn05-1.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 class graph{
 	int **l ;
-	int n , rec[2][n] ;
+	int n ;
+	int rec[2][10] ;
 public:
 	graph(int n){
 		this->n = n ;
@@ -30,12 +31,7 @@ public:
 	void create(){
 		int cont = 1 ;
 		while(cont == 1){
-			int of1,of2,cost ;
-			cout<<"\nEnter office numbers to be connected by phone line :" ;
-			cin>>of1>>of2 ;
-			cout<<"\nCost of connection :" ;
-			cin>>cost ;
-			l[of1-1][of2-1] = l[of2-1][of1-1] = cost ;
+			add() ;
 			cout<<"Do you wish to add more phone lines (1/0):" ;
 			cin>>cont ;
 		}
@@ -43,18 +39,33 @@ public:
 
 	void add(){
 		int of1,of2,cost ;
-		cout<<"\nEnter office numbers to be connected by phone line :" ;
+		cout<<"Enter office numbers to be connected by phone line :"<<endl ;
 		cin>>of1>>of2 ;
-		cout<<"\nCost of connection :" ;
+		cout<<"Cost of connection :" ;
 		cin>>cost ;
-		l[of1-1][of2-1] = l[of2-1][of1-1] = cost ;
+		l[of1-1][of2-1] = cost ;
+		l[of2-1][of1-1] = cost ;
 	}
 
 	void prims(){
-
+		int j = 0 ,i = 0 ;
+		while(check_rec() != -1){
+			for(int m = 0 ;m<n;m++){
+				if(l[i][m] != 0 && rec[0][m] != -1){
+					rec[0][m] = l[i][m] ;
+					rec[1][m] = i ;
+				}
+			}
+			cout<<endl<<"Phone line between offices "<<min(1) + 1<<" and "<<rec[1][min(1)] + 1
+				<<" with a cost of "<<min(0) ;
+			j += min(0) ;
+			rec[0][min(1)] = -1 ;
+			i++ ;
+		}
+		cout<<endl<<"This set of Phone lines connect all the offices with minimum cost of "<<j ;
 	}
 
-	int check_rec01(){
+	int check_rec(){
 		int flag = 0 ;
 		for(int i = 0 ;i<n;i++){
 			if(rec[0][i] == -1)
@@ -64,8 +75,47 @@ public:
 			return -1 ;
 		return 0 ;
 	}
+	
+	int min(int decide){
+		int mink = 1000 ,index = 0 ;
+		for(int i = 0;i<n;i++){
+			if(rec[0][i] != -1 && rec[0][i] < mink ){
+				mink = rec[0][i] ;
+				index = i ;
+			} 
+		}
+		if(decide = 0 )
+			return mink ;
+		else
+			return index ;
+	}
 };
 
 int main() {
+	int nn ;
+	cout<<endl<<"Enter total no. of offices : " ;
+	cin>>nn ;
+	graph *network ;
+	network = new graph(nn) ;
+	int ch ;
+	do{
+		cout<<endl<<"Press"
+			<<endl<<"1 to create the network "
+			<<endl<<"2 to add a phone line "
+			<<endl<<"3 to claculate set of lines for minimum cost(Prim's)"
+			<<endl<<"4 to Exit" ;
+		cin>>ch ;
+		switch(ch){
+			case 1 :
+				network->create() ;
+				break ;
+			case 2:
+				network->add() ;
+				break ;
+			case 3:
+			 	network->prims() ;
+			 	break ;
+		}
+	}while(ch != 4) ;
 	return 0;
 }
