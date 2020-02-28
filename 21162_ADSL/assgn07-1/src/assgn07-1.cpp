6720077b2b54a38828869af77d88bfedc45fcd21
aditya332 <50880895+aditya332@users.noreply.github.com>
2020-02-28 15:42:47 +0530

    assgn06-1

---
+++ 21162_ADSL/assgn07-1/src/assgn07-1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : assgn07-1.cpp
+// Author      : adi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
---
+++ 21162_ADSL/assgn08-1/src/assgn08-1.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : assgn08-1.cpp
+// Author      : adi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class node{
+	string key ;
+	int p , q ;
+public :
+	node(){
+		this->key[0] = '\0' ;
+		this->p = 0 ;
+		this->q = 0 ;
+	}
+};
+
+int main() {
+
+	return 0;
+}
