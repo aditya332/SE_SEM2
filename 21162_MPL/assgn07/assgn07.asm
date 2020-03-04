58ba9e659f38774e1293ccda68538c7318dfc241
aditya332 <50880895+aditya332@users.noreply.github.com>
2020-03-04 15:35:33 +0530

    till assgn07

--- 21162_MPL/assgn07/assgn07.asm
+++ 21162_MPL/assgn07/assgn07.asm
@@ -200,31 +200,4 @@ _ProcessArray:
 	jnz begin1
 	
 	ret;
-		
-		
-		
-_HexToAscii:;		// HEX in RAX -----> ASCII in outAscii
-    mov rsi,outAscii+3d;
-    mov rcx,4d
-
-    begin2:
-    xor rdx,rdx;
-    mov rbx,10h;	//16d
-    div rbx;
-
-    cmp dl,09h;
-    jbe add30;
-    add dl,07h;
-    add30:
-    add dl,30h;
-    mov byte[rsi],dl;
-
-    update:
-    dec rsi;
-    dec rcx;
-    jnz begin2;
-    ret
-	
-		
-
 
